ffe4b533c7742d854644ac26962db06bfdb2c01c
WilliamHuey <lliw1236@yahoo.com>
2013-12-03 16:50:55 -0800

    redesigning

--- js/main.js
+++ js/main.js
@@ -1,76 +1,83 @@
 ﻿$(function(){
 
+  var $flySubMenus = $('ul.fly-out');
+  $flySubMenus.addClass('sub-level');
+
+  var $mainMenu = $('#main-nav-outer');
+
+  function createFlyoutMenu() { $mainMenu.menu(); }
+  
+  createFlyoutMenu();   
+
   var innerWidth,
-    innerHeight,
-    adjImgHeight,        
-    res = (16/9),
-    bg = $('#bg'),
+    $document = $(document),
     $theWindow = $(window),
-    lastWindowWidth = window.innerWidth,
+    lastWindowWidth = $theWindow.width(),
+    lastWindowHeight = $theWindow.height(),
     $ulNav = $('ul.main-nav-ul'),
     breakPoint = 600,
     largerThanBreak = breakPoint + 1,
-    clickStatus;
-  
-  
-  $theWindow.resize(function() {
-  
-    //For background image resizing
-    innerWidth = window.innerWidth;
-    innerHeight = window.innerHeight;
-    adjImgHeight = innerWidth/res,
-    adjImgHeightMult = innerHeight * res;
-    //Width is way larger or equal to the height
-    //Resize image to fit the width
-    if(innerWidth >= innerHeight) {
-      //When the adjusted image height is less
-      //than the window height, fill the adjusted
-      //image to the height of the window
-      if(adjImgHeight <= innerHeight) {
-        bg.css('height', (innerHeight + 'px'));
-        bg.css('width', (adjImgHeightMult + 'px'));
-      } else {
-        bg.css('width', '100%');
-        bg.css('height', (adjImgHeight + 'px'));
+    vertBreakPoint = 250,
+    greaterVertBreakPoint = vertBreakPoint + 1,
+    clickStatus = false,
+    $navButton = $( "#nav-button" ),
+    flyOutMenuPresent = true;
+
+  function checkBreakPoint(dim, lastdim, breakPoint, status ,elem) {
+   if( (dim <= lastdim) && (dim < breakPoint)) {
+      //console.log('width hide');
+      if(!status) {
+        elem.hide();
       }
-    } else if(innerWidth < innerHeight) {
-      //Length is way larger than the width
-      //Adjust the image to fit height
-      bg.css('height', (innerHeight + 'px'));
-      bg.css('width', (adjImgHeightMult + 'px'));
     }
-    
-    //For the menu restoration
-    //If hidden by click and the window is getting larger
-    if ( $ulNav.is( ":hidden" ) ) {
-      if(innerWidth > lastWindowWidth) {
-        if(innerWidth >= largerThanBreak) {
-          console.log('show');
-          //Restore if hidden        
-          $ulNav.show(); 
-        }
-      }
+  }
+  
+  $theWindow.on("resize.kdSite", function() {    
+   innerWidth = $theWindow.width();
+   innerHeight = $theWindow.height();
+
+   //Nav button is not hidden
+    if(!$navButton.is( ":hidden" )) {
+      //Have to remove flyout menu if nav button present
+      if(flyOutMenuPresent) {
+        $mainMenu.menu('destroy');        
+        $mainMenu
+          .children()
+          .children("ul.sub-nav-ul")
+          .removeClass( "sub-level" );
+        
+        flyOutMenuPresent = false; 
+      }      
+      
+      //Less than width breakpoint or less than height breakpoint
+      //Also make sure the nav button was not clicked before
+      //Hide the menus            
+      checkBreakPoint(innerWidth, lastWindowWidth, breakPoint, clickStatus, $ulNav);
+      checkBreakPoint(innerHeight, lastWindowHeight, breakPoint, clickStatus, $ulNav);
+
     } else {
-      //Menu was visible before when larger
-      //Now window width is smaller, now hide the menu
-      if(innerWidth <= breakPoint && lastWindowWidth >= largerThanBreak) {
-        //Also check if menu was already visible due to click of button
-        //If that was the case do not hide the menu
-        if(clickStatus === false) {
-          $ulNav.hide(); 
-        }
+      //Restore the flyout if it is not present
+      if(!flyOutMenuPresent) {
+        $flySubMenus.addClass('sub-level');
+        //Bring the flyout menu back
+        createFlyoutMenu();
+        flyOutMenuPresent = true;                
+      }      
+      if($ulNav.is( ":hidden" )) {
+        $ulNav.show();
       }
     }
     
-    //Current width is not the last window width
-    lastWindowWidth = innerWidth;
+    //Current width/height is now the last window width/height
+    lastWindowWidth = innerWidth;    
+    lastWindowHeight = innerHeight;
   });
   
   //Trigger for performing adjustments on browser load
-  $theWindow.trigger('resize'); 
-
+  $theWindow.trigger('resize.kdSite'); 
+  
   //For the menu button click action  
-  $( "#nav-button" ).on('click', function() {
+  $navButton.on('click.kdSite', function() {
     if ( $ulNav.is( ":hidden" ) ) {
       $ulNav.slideDown();
       clickStatus = true;
@@ -79,5 +86,5 @@
       clickStatus = false;
     }
   });  
-  
+ 
 });
