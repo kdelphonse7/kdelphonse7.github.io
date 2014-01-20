7ec5557609d313f7a4ea254ef952752e027167b2
WilliamHuey <lliw1236@yahoo.com>
2014-01-20 12:23:35 -0800

    gallery adjustments, utils js for other pages, name correction on index

--- js/photography/galleriaRun.js
+++ js/photography/galleriaRun.js
@@ -1,22 +1,39 @@
- // Load the classic theme
-Galleria.loadTheme('js/photography/galleria.classic.js');
+ $(function () {
+   // Load the classic theme
+   Galleria.loadTheme('js/photography/galleria.classic.js');
 
-$(window).ready(function() {      
-  // Initialize Galleria
-  Galleria.run('#galleria');
-  
-  var styleSelector = '#main-styles';
-  //var mainRules = u.ss.getSS(styleSelector);
-  
-  console.log(u.ss.getS());
-  
-  var windowHeight = $(this).height();
-  u.ss.addRule("#galleria", "{height:" + windowHeight*0.90 + "px}");
-  
-  
-  //console.log(mainRules);
-  
-  
-  //var ruleNumber = u.ss.getSSRule(mainRules, "#main-nav ul li:hover > ul");
-  //u.ss.deleteRule(ruleNumber);
-});
+   function calculateGalleriaHeight() {
+     windowHeight = $window.height();
+     windowHeight = determineHeight(windowHeight);
+     $galleria.css("height", (windowHeight * factor + "px"));
+   }
+   
+   function determineHeight(windowHeight) {
+     if(windowHeight <= 250) {
+       return 250;
+     } else {
+       return windowHeight;
+     }
+   }
+
+   var $window = $(window),
+     $galleria = $('#galleria'),
+     factor = 0.90,
+     windowHeight;
+
+   //Run and calculate height of Galleria
+   $window.ready(function () {
+     //windowHeight = $(this).height();
+     calculateGalleriaHeight();
+     // Initialize Galleria
+     Galleria.run('#galleria');
+     //Set height of Galleria on load
+     $('head').append('<style id="js-gen-galleria">#galleria {height:' +
+       windowHeight * factor + 'px}</style>');
+
+   }).on('resize.kdPhotography', function () {
+     //Resize the Galleria height on resize
+     calculateGalleriaHeight();
+   });
+
+ });
