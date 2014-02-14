fd2a9281535456427db89a4e52595948458c03e6
WilliamHuey <lliw1236@yahoo.com>
2014-02-13 17:09:19 -0800

    png fallback for philosophy, index changes for old ie

---
+++ js/oldIe-Index.js
@@ -0,0 +1,21 @@
+﻿$(function () {
+  
+  
+  function adjustMargin() {
+    var wWidth = $(window).width(),
+  $vBg = $(".video-background"),
+  vWidth = $vBg.width(),
+  mL = wWidth - vWidth;
+    if (mL > 0) {
+      $(".video-background").css({
+        marginLeft : ((mL / 2) + 'px')
+      });
+    }
+  }
+  adjustMargin();
+  $(window).on('resize', function() {
+    adjustMargin();
+  });
+  
+  
+});
